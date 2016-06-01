2338591e9dd4d8493b3dce8b8d4c3442f866af8b
Mary Rose Cook <mary@maryrosecook.com>
2016-06-01 19:05:38 +0100

    Set initial content of promises index.js to log “hi!”

--- promises-workshop/index.js
+++ promises-workshop/index.js
@@ -1,10 +1,1 @@
-function after(delay) {
-  return new Promise(function(resolve) {
-    setTimeout(resolve, delay);
-  });
-};
-
-after(1000)
-  .then(function() {
-    console.log("Surprise!");
-  })
+console.log("hi!");
